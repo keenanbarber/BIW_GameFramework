91b0b054637e0043f6ffccd097c0dad9fe8cdfa7
Barber, Keenan <Keenan.Barber@biworldwide.com>
2017-06-14 16:19:11 -0500

    Added some code that might or might not work? beginSwipe() and endSwipe()

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,6 +78,54 @@ var Game = {
 
 	test: function() {
 		console.log("This is a test.");
+	}, 
+
+	// when the player begins to swipe we only save mouse/finger coordinates, remove the touch/click
+	// input listener and add a new listener to be fired when the mouse/finger has been released,
+	// then we call endSwipe function
+	beginSwipe: function(){
+		startX = game.input.worldX;
+		startY = game.input.worldY;
+		game.input.onDown.remove(beginSwipe);
+     	game.input.onUp.add(endSwipe);
+	},
+	
+	// function to be called when the player releases the mouse/finger
+	endSwipe: function(){
+		// saving mouse/finger coordinates
+		endX = game.input.worldX;
+		endY = game.input.worldY;
+		// determining x and y distance travelled by mouse/finger from the start
+		// of the swipe until the end
+		var distX = startX-endX;
+		var distY = startY-endY;
+		// in order to have an horizontal swipe, we need that x distance is at least twice the y distance
+		// and the amount of horizontal distance is at least 10 pixels
+		if(Math.abs(distX)>Math.abs(distY)*2 && Math.abs(distX)>10){
+			// moving left, calling move function with horizontal and vertical tiles to move as arguments
+			if(distX>0){
+                    move(-1,0);
+               }
+               // moving right, calling move function with horizontal and vertical tiles to move as arguments
+               else{
+                    move(1,0);
+               }
+		}
+		// in order to have a vertical swipe, we need that y distance is at least twice the x distance
+		// and the amount of vertical distance is at least 10 pixels
+		if(Math.abs(distY)>Math.abs(distX)*2 && Math.abs(distY)>10){
+			// moving up, calling move function with horizontal and vertical tiles to move as arguments
+			if(distY>0){
+                    move(0,-1);
+               }
+               // moving down, calling move function with horizontal and vertical tiles to move as arguments
+               else{
+                    move(0,1);
+               }
+		}	
+		// stop listening for the player to release finger/mouse, let's start listening for the player to click/touch
+		game.input.onDown.add(beginSwipe);
+     	game.input.onUp.remove(endSwipe);
 	}
 
 };
